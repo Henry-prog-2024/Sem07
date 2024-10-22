91873541d15f332b82185d3a4090899e4cd33bec
Henry-prog-2024 <i2320225@continental.edu.pe>
2024-10-22 09:29:48 -0500

    codigo completo

---
+++ Sem02.py
@@ -0,0 +1,31 @@
+import streamlit as st
+
+#Inicialización de variables
+usuarios = []
+
+#Funcion para agragar un usuario
+def agregar_usuario(nombre):
+    usuarios.append(nombre)
+    st.success(f"Usuario:{nombre} agregado")
+
+    #Funcion para mostrar usuarios
+    def mostrar_usuarios()
+    if usuarios:
+        st.write("Lista de usuarios")
+        for usuario in usuario:
+    else
+        st.warning("No hay usuarios registrados: ")
+#Menu principal
+st.title("Gestión de usuarios")
+option = st.selectbox("Selecciona una opción: ["Agregar usuasio, Mostrar usuario"])
+if option -- "Agregar usuario":
+    nombre = st.text_input ("Nombre del usuario: ")
+    if st.botton("Agregar"):
+        if nombre:
+            agregar_usuario(nombre)
+        else:
+            st.error("El nombre no puede estar vacio")
+elif option == "Mostrar usuario"
+    mostrar_usuario()
+
+
