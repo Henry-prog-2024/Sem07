fc5259083f60c4d82d394c8dec463744d27f6b2d
Henry-prog-2024 <i2320225@continental.edu.pe>
2024-10-22 11:57:51 -0500

    Listo

--- Sem02.py
+++ Sem02.py
@@ -30,6 +30,6 @@ if opcion == "Agregar Usuario":
         else:
             st.error("El nombre no puede estar vacío")
 
-elif opcion == "Mostrar Usuarios":
+elif opcion == "Mostrar Usuario":
     if st.button("Mostrar usuarios"):
         mostrar_usuarios()
