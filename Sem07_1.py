ebc2c9a7764274afecf6fbecee55a1e6cd3cafdf
Henry-prog-2024 <i2320225@continental.edu.pe>
2024-10-23 19:09:51 -0500

    Listo

--- Sem07_1.py
+++ Sem07_1.py
@@ -2,36 +2,36 @@ import streamlit as st
 
 #Función principal para verificar automóviles
 def verificar_automoviles():
-    st.title("Centro de verificación de automóviles")
+    st.title("Centro de Verificación de Automóviles")
 
-    #Lista para verificar los puntos contaminantes.
-    If 'puntos_contaminantes' not in st.session_state:
-        st.session_state.puntos_contaminantes =[]
+    #Lista para almacenar los puntos contaminantes
+    if 'puntos_contaminantes' not in st.session_state:
+        st.session_state.puntos_contaminantes = []
 
-    # Input para los puntos contaminanttes del automovil
-    puntos = st.numbers_input("Ingrese los puntos fundamentales del automovil", min_value=0.0, step=0.1)
+    #Input para los puntos contaminantes del automóvil
+    puntos = st.number_input("Ingrese los puntos contaminantes del automóvil", min_value=0.0, step=0.1)
 
-    # Botón para registrar el automovil
-    if st.button("Registrar automovil"):
+    #Botón para registrar el automóvil
+    if st.button("Registrar automóvil"):
         st.session_state.puntos_contaminantes.append(puntos)
-        st.success(f"Automovil registrado con {puntos} putos contaminantes.")
+        st.success(f"Automóvil registrado con {puntos} puntos contaminates.")
 
-    # Mostrar los datos registrados hasta el momento
-    if len(st.session_state.puntos_contaminantes) > 0 and st.button("calcular resultados"):
-        promedio=sum(st.session_state.puntos_contaminantes)/len(st.session_state.puntos_contaminantes)
+    #Mostrar los datos registrado hasta el momento
+    if len(st.session_state.puntos_contaminantes) > 0 and st.button("Calcular resultados"):
+        promedio = sum(st.session_state.puntos_contaminantes) / len(st.session_state.puntos_contaminantes)
         menos_contaminacion = min(st.session_state.puntos_contaminantes)
         mas_contaminacion = max(st.session_state.puntos_contaminantes)
 
-        # Mostrar resultados
+        #Mostrar los resultados
         st.write(f"Promedio de puntos contaminantes: {promedio:.2f}")
-        st.write(f"El automovil que menos contamino tiene: {menos_contaminacion:.2f}")
-        st.write(f"El automovil que más contaminó tiene: {mas_contaminacion:.2f}")
-
+        st.write(f"El automóvil que menos contaminó tiene {menos_contaminacion}")
+        st.write(f"El automóvil que más contaminó tiene {mas_contaminacion}")
+    
     #Opción para reiniciar los datos
     if st.button("Reiniciar datos"):
-        st.seccion_state.puntos_contaminantes = []
-        st.susses("Datos reiniciados correctamente")
+        st.session_state.puntos_contaminantes = []
+        st.success("Datos reiniciados correctamente")
 
 #Ejecutar la función
-if __name__ == "__main__"
-    verificar automoviles()
+if __name__ == "__main__":
+    verificar_automoviles()
