f889f4d79e1934c050e77b34e4bbf1305506e8bc
Henry-prog-2024 <i2320225@continental.edu.pe>
2024-10-28 21:46:45 -0500

    Semana8

---
+++ Sem08.py
@@ -0,0 +1,28 @@
+import streamlit as st 
+def validate_data(marca, modelo, kilometraje):
+    """Valida los datos ingresados para el automóvil"""
+    if not marca or not modelo:
+        return  "La marca y el modelo no deben de estar vacios."
+    try:
+        kilometraje = float(kilometraje)
+        if kilometraje < 0:
+            return "El kilometraje no puede ser menor de 0."
+        except valueError:
+            return "Ell kilometraje debe ser un número válido."
+        return None
+
+def main()
+    st.title("Registro de automovil")
+    st.write("Ingrese los datos del automóvil a continuación")
+    ##Registro por el usuario
+    error = validate_data(marca, modelo, kilometraje)
+    if error:
+        st.error(error)
+    else:
+        st.success("Automovil registrado exitosamente.")
+        st.write("**Marca**", marca)
+        st.write("**Modelo**", modelo)
+        st.write("**kilometraje**", kilometraje)
+
+if __name__ == "__main__":
+    main()
