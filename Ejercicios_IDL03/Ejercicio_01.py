6608a0fed6c6f5e63d92d5f8a33002cfa35099de
Henry-prog-2024 <i2320225@continental.edu.pe>
2024-10-31 20:37:45 -0500

    Listo

--- Ejercicios_IDL03/Ejercicio_01.py
+++ Ejercicios_IDL03/Ejercicio_01.py
@@ -3,19 +3,26 @@ import streamlit as st
 #cero. Debe imprimir la suma y la media de todos los números
 #introducidos.
 
+import streamlit as st
+
 def main():
+    st.title("Calcular SUMA y MEDIA")
+    
+    numeros = []
+    
     while True:
-        numero = st.text_input("Introduce un número (0 para terminar):")
-        numero = float(numero)
+        numero = st.number_input("Introduce un número (0 para terminar):", step=1.0)
+        
         if numero == 0:
             break
-        
-        suma = suma + numero
-        contador = contador + 1
-    if contador > 0:
-        media = suma / contador
+            
+        numeros.append(numero)
+
+    if numeros:
+        suma = sum(numeros)
+        media = suma / len(numeros)
         st.write(f"Suma: {suma}")
-        st.write(f"Media: {suma}")
+        st.write(f"Media: {media}")
     else:
         st.write("No se introdujeron números.")
 
