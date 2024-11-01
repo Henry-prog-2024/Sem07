5d74e8fd0b7f3d53518de5e1cd3e3e4f2d9163be
Henry-prog-2024 <i2320225@continental.edu.pe>
2024-10-31 20:09:07 -0500

    Ejer01

---
+++ Ejercicios_IDL03/Ejercicio_01.py
@@ -0,0 +1,25 @@
+import streamlit as st
+#Algoritmo que pida números hasta que se introduzca un
+#cero. Debe imprimir la suma y la media de todos los números
+#introducidos.
+
+def main():
+    suma = 0
+    contador = 0
+
+    while True:
+        numero = st.text_input("Introduce un número (0 para terminar): ")
+        if numero == 0:
+            break
+        
+        suma += numero
+        contador += 1
+    if contador > 0:
+        media = suma / contador
+        st.write(f"Suma: {suma}")
+        st.write(f"Media: {suma}")
+    else:
+        st.write("No se introdujeron números.")
+
+if __name__ == "__main__":
+    main()
