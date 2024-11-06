2683ab17cada1eaa7477a76abc86d1ea733d5fb0
Henry-prog-2024 <i2320225@continental.edu.pe>
2024-11-05 19:38:40 -0500

    Listo

--- Ejercicios_IDL03/Ejercicio_01.py
+++ Ejercicios_IDL03/Ejercicio_01.py
@@ -1,7 +1,13 @@
 import streamlit as st
-st.title("Este programa es mío")
-num1 = st.number_input("ingrese primer número", value=0)
-num2 = st.number_input("ingrese segundo número", value=0)
-suma = num1 + num2
+
+def suma(num1, num2):
+    return num1 + num2
+
+def main()
+    st.title("Este programa es mío")
+    num1 = st.number_input("ingrese primer número", value=0)
+    num2 = st.number_input("ingrese segundo número", value=0)
+    suma = calcular(num1, num2)
+
 st.write(f"la suma es: {suma}")
 
