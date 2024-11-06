2ebde327de5f7de3d425c52253db266151028d81
Henry-prog-2024 <i2320225@continental.edu.pe>
2024-11-05 19:19:09 -0500

    Error sintaxis

--- Ejercicios_IDL03/Ejercicio_01.py
+++ Ejercicios_IDL03/Ejercicio_01.py
@@ -1,7 +1,7 @@
 import streamlit as st
 st.title("ESte programa es mío")
-num1 = st.numero_input("ingrese primer número")
-num2 = st.numero_input("ingrese segundo número")
+num1 = st.number_input("ingrese primer número")
+num2 = st.number_input("ingrese segundo número")
 sum= sum1 + sum2
 st.write(f"la suma es: {sum}")
 
