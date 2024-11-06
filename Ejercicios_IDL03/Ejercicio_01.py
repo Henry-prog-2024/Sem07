64cf8ad799b806e3ee8694d0457e8a3b85f3d6d5
Henry-prog-2024 <i2320225@continental.edu.pe>
2024-11-05 19:40:32 -0500

    Listo

--- Ejercicios_IDL03/Ejercicio_01.py
+++ Ejercicios_IDL03/Ejercicio_01.py
@@ -7,7 +7,7 @@ def main():
     st.title("Este programa es mío")
     num1 = st.number_input("ingrese primer número", value=0)
     num2 = st.number_input("ingrese segundo número", value=0)
-    suma = calcular(num1, num2)
+    suma = suma(num1, num2)
 
 st.write(f"la suma es: {suma}")
 
