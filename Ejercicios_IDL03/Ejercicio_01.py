266c2cfa7c89f8765267034fff0f1064c8d2aa6a
Henry-prog-2024 <i2320225@continental.edu.pe>
2024-11-06 11:56:02 -0500

    ok

--- Ejercicios_IDL03/Ejercicio_01.py
+++ Ejercicios_IDL03/Ejercicio_01.py
@@ -6,10 +6,10 @@ def suma(num1, num2):
 def resta(num1, num2):
     return num1 - num2
 
-def multiplicar(num1 * num2):
+def multiplicar(num1,num2):
     return num1 * num2
 
-def dividir(num1 * num2):
+def dividir(num1, num2):
     return num1/num2
 
 def main():
