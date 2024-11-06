d7c7a011b787d5e9304c71382e8359d72b2c1f02
Henry-prog-2024 <i2320225@continental.edu.pe>
2024-11-05 19:51:35 -0500

    Boton

--- Ejercicios_IDL03/Ejercicio_01.py
+++ Ejercicios_IDL03/Ejercicio_01.py
@@ -7,8 +7,9 @@ def main():
     st.title("Este programa es mío")
     num1 = st.number_input("ingrese primer número", value=0)
     num2 = st.number_input("ingrese segundo número", value=0)
-    resultado = suma(num1, num2)
-    st.write(f"la suma es: {resultado}")
+    if st.button("Suma"):
+        resultado = suma(num1, num2)
+        st.write(f"la suma es: {resultado}")
 
 if __name__ == "__main__":
     main()
